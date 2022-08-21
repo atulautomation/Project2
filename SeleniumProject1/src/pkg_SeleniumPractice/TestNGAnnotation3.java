4dd03c543817fcd6fdf33b4d9bf94ef026860c4c
Atul <atulingle05@gmail.com>
2022-08-21 20:42:04 +0530

    Selenium code

---
+++ SeleniumProject1/src/pkg_SeleniumPractice/TestNGAnnotation3.java
@@ -0,0 +1,36 @@
+package pkg_SeleniumPractice;
+
+import org.testng.annotations.Test;
+
+public class TestNGAnnotation3 {
+
+	@Test
+	public void TC_01()
+	{
+		System.out.println("This is first APPIUM test");
+	}
+	
+	@Test
+	public void TC_02()
+	{
+		System.out.println("This is second APPIUM test");
+	}
+	
+	@Test
+	public void TC_03()
+	{
+		System.out.println("This is third APPIUM test");
+	}
+	
+	@Test
+	public void TC_04()
+	{
+		System.out.println("This is fourth APPIUM test");
+	}
+	
+	@Test
+	public void TC_05()
+	{
+		System.out.println("This is fifth APPIUM test");
+	}
+}
